57abfc10e7687a40cab5696b626ef71e14ccf6dd
nitheshkumarn <nitheshkn012@gmail.com>
2024-07-02 01:09:17 +0530

    finished all entuty classes

---
+++ src/main/java/com/hackathon/ai/controller/HealthDataController.java
@@ -0,0 +1,5 @@
+package com.hackathon.ai.controller;
+
+public class HealthDataController {
+
+}
--- src/main/java/com/hackathon/ai/entity/User.java
+++ src/main/java/com/hackathon/ai/entity/User.java
@@ -1,5 +1,46 @@
 package com.hackathon.ai.entity;
 
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Builder
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer userId;
+	
+	private String userName;
+	private String userPass;
+	private String userEmail;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private Set<HealthData> healthData;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private Set<Goal> goals;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private Set<Recommendation> recommendations;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private Set<Notification> notifications;
+	
 
 }
